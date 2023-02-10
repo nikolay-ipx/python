0112ca5ce727406d993644c612a1475199583b71
user <nik>
2023-02-10 18:15:08 +0300

    1

--- 3/3_1_5.py
+++ 3/3_1_5.py
@@ -16,10 +16,43 @@ class LessonItem:
         return False
 
     def __delattr__(self, item):
-        if item in (self.title, self.practices, self.duration):
+        if item in self.a:
             return
+        else:
+            object.__delattr__(self, item)
 
 
-c=LessonItem('2',1,3)
-del c.title
-print(c.__dict__)
+class Module:
+    def __init__(self, name):
+        self.name = name
+        self.lessons = []
+
+    def add_lesson(self, lesson):
+        self.lessons.append(lesson)
+
+    def remove_lesson(self, indx):
+        self.lessons.pop(indx)
+
+class Course:
+    def __init__(self, name):
+        self.name = name
+        self.modules = []
+
+    def add_module(self, module):
+        self.modules.append(module)
+
+    def remove_module(self, indx):
+        self.modules.pop(indx)
+
+
+
+course = Course("Python ООП")
+module_1 = Module("Часть первая")
+module_1.add_lesson(LessonItem("Урок 1", 7, 1000))
+module_1.add_lesson(LessonItem("Урок 2", 10, 1200))
+module_1.add_lesson(LessonItem("Урок 3", 5, 800))
+course.add_module(module_1)
+module_2 = Module("Часть вторая")
+module_2.add_lesson(LessonItem("Урок 1", 7, 1000))
+module_2.add_lesson(LessonItem("Урок 2", 10, 1200))
+course.add_module(module_2)
